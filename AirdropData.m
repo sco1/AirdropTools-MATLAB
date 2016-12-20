5aa2e5cc92ee5a489b2dd89946cc9ad2afd82811
S. Co1 <sco1.git@gmail.com>
2016-12-20 11:32:54 -0500

    Add cross-platform recursive directory search

--- AirdropData.m
+++ AirdropData.m
@@ -196,6 +196,77 @@ classdef AirdropData < handle
             delete(dragpatch)
             fig.WindowButtonUpFcn = oldbuttonup;
         end
+        
+        function [varargout] = subdir(varargin)
+            narginchk(0,1);
+            nargoutchk(0,1);
+            
+            if nargin == 0
+                folder = pwd;
+                filter = '*';
+            else
+                [folder, name, ext] = fileparts(varargin{1});
+                if isempty(folder)
+                    folder = pwd;
+                end
+                if isempty(ext)
+                    if isdir(fullfile(folder, name))
+                        folder = fullfile(folder, name);
+                        filter = '*';
+                    else
+                        filter = [name ext];
+                    end
+                else
+                    filter = [name ext];
+                end
+                if ~isdir(folder)
+                    error('Folder (%s) not found', folder);
+                end
+            end
+            
+            %---------------------------
+            % Search all folders
+            %---------------------------
+            
+            pathstr = AirdropData.genpath_local(folder);
+            pathfolders = regexp(pathstr, pathsep, 'split');  % Same as strsplit without the error checking
+            pathfolders = pathfolders(~cellfun('isempty', pathfolders));  % Remove any empty cells
+            
+            Files = [];
+            pathandfilt = fullfile(pathfolders, filter);
+            for ifolder = 1:length(pathandfilt)
+                NewFiles = dir(pathandfilt{ifolder});
+                if ~isempty(NewFiles)
+                    fullnames = cellfun(@(a) fullfile(pathfolders{ifolder}, a), {NewFiles.name}, 'UniformOutput', false);
+                    [NewFiles.name] = deal(fullnames{:});
+                    Files = [Files; NewFiles];
+                end
+            end
+            
+            %---------------------------
+            % Prune . and ..
+            %---------------------------
+            
+            if ~isempty(Files)
+                [~, ~, tail] = cellfun(@fileparts, {Files(:).name}, 'UniformOutput', false);
+                dottest = cellfun(@(x) isempty(regexp(x, '\.+(\w+$)', 'once')), tail);
+                Files(dottest & [Files(:).isdir]) = [];
+            end
+            
+            %---------------------------
+            % Output
+            %---------------------------
+            
+            if nargout == 0
+                if ~isempty(Files)
+                    fprintf('\n');
+                    fprintf('%s\n', Files.name);
+                    fprintf('\n');
+                end
+            elseif nargout == 1
+                varargout{1} = Files;
+            end
+        end
     end
     
     methods (Static, Hidden, Access = protected)
@@ -346,9 +417,40 @@ classdef AirdropData < handle
         end
         
         function checkdragwindowwidth(ed, width)
+            % Check the width of the drag patch to make sure it doesn't get
+            % adjusted. For a still-unknown reason there is a scenario where
+            % the width of the patch grows unbounded. It seems to be 
+            % triggered by the data boundary check on drag.
             badwidth = abs(ed.AffectedObject.XData(1) - ed.AffectedObject.XData(2)) ~= width;
             if badwidth
                 ed.AffectedObject.XData(2:3) = [1, 1]*ed.AffectedObject.XData(1) + width;
+            end
+        end
+        
+        function [p] = genpath_local(d)
+            % Modified genpath that doesn't ignore:
+            %     - Folders named 'private'
+            %     - MATLAB class folders (folder name starts with '@')
+            %     - MATLAB package folders (folder name starts with '+')
+            
+            files = dir(d);
+            if isempty(files)
+                return
+            end
+            p = '';  % Initialize output
+            
+            % Add d to the path even if it is empty.
+            p = [p d pathsep];
+            
+            % Set logical vector for subdirectory entries in d
+            isdir = logical(cat(1,files.isdir));
+            dirs = files(isdir);  % Select only directory entries from the current listing
+            
+            for i=1:length(dirs)
+                dirname = dirs(i).name;
+                if    ~strcmp( dirname,'.') && ~strcmp( dirname,'..')
+                    p = [p genpath(fullfile(d,dirname))];  % Recursive calling of this function.
+                end
             end
         end
     end
